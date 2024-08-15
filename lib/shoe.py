4a7e748eea77a0b027706babbaf61171341f7bbe
AMIRKIAYI <amirkiayi516@gmail.com>
2024-08-15 14:19:40 +0300

    ready files

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        self.size = size
+        self.condition = "New"
+
+    def repair(self):
+        self.condition = "Repaired"
+        print("The shoe has been repaired.")
